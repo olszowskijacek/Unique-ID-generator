bc5651f7e9766cadd670f2de0208067077191e09
Jacek Olszowski <jacek.olszowski1@gmail.com>
2020-03-25 16:05:31 +0100

    fix

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-const randomId = () => {
+const randomID = idLength => {
     let id = '';
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const characters =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     const charsAmount = characters.length;
-    for(let i = 0; i < idLength; i++) {
-        id += characters.charAt(Math.floor(Math.random() * charsAmount));
+    for (let i = 0; i < idLength; i++) {
+      id += characters.charAt(Math.floor(Math.random() * charsAmount));
     }
     return id;
-
-}
-
-
-module.exports = randomID;
+  };
+  
+  module.exports = randomID;
